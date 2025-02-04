e1fa2d3023b98a5acd14ef2df3dd1255ad882cd3
Thrashbang <159561923+Thrashbang@users.noreply.github.com>
2025-02-04 09:24:17 -0700

    Create jackbox-games-room-finder.py

---
+++ jackbox-games-room-finder.py
@@ -0,0 +1,11 @@
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+for letter1 in alphabet:
+
+  for letter2 in alphabet:
+
+    for letter3 in alphabet:
+
+      for letter4 in alphabet:
+
+        
